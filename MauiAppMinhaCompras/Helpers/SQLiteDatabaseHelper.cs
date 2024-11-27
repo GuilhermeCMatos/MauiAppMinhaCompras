381683acd032819f5f74312c2436b5a90c02aee5
user <user@DESKTOP-AOMDCF4>
2024-11-27 14:06:51 -0300

    AULA 10

--- MauiAppMinhaCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhaCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -1,9 +1,5 @@
 ﻿using MauiAppMinhaCompras.Models;
 using SQLite;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MauiAppMinhaCompras.Helpers
 {
@@ -14,6 +10,38 @@ namespace MauiAppMinhaCompras.Helpers
         {
             _conn = new SQLiteAsyncConnection(path);
             _conn.CreateTableAsync<Produto>().Wait();
+        }
+
+        public Task<int> Insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+
+        public Task<List<Produto>> Update(Produto p) 
+        {
+            string sql = "UPDATE Produto SET Descricao=?, Quantidade = ?, Preço = ? WHERE id=?"
+
+            return _conn.QueryAsync<Produto>(
+                sql, p.Descricao, p.Quantidade, p.Preco, p.Id
+                );
+        }
+
+        public Task<int> Delete(int id) 
+        {
+            return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+
+        public Task<List<Produto>> GetAll() 
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public Task<List<Produto>> Search(string q) 
+        {
+            string sql = "SELECT * Produto WHERE descricao LIKE '%" + q + "%'";
+
+            return _conn.QueryAsync<Produto>(sql);
+                
         }
     }
 }

10e02ca72e383535ef23e97451a601ecfc7f4134
Usuario <Usuario@CPD03-M19>
2024-11-28 14:06:46 -0300

    AULA 11

--- MauiAppMinhaCompras/App.xaml.cs
+++ MauiAppMinhaCompras/App.xaml.cs
@@ -2,6 +2,25 @@
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabeseHelper Db
+        {
+            get 
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabeseHelper(".... db3");
+                }
+
+                return _db;
+        }
+    }
         public App()
         {
             InitializeComponent();
--- MauiAppMinhaCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhaCompras/Views/ListaProduto.xaml.cs
@@ -6,4 +6,16 @@ public partial class ListaProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void TollbarItem_Clicked(object sender, EventArgs e)
+	{
+		try
+		{
+			Navegation.PushAsync(new Views.NovoProduto());
+		}
+		catch (Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "OK");
+		}
+	}
 }
